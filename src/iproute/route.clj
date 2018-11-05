07587c9695d04a24449866772c41d5ebf064375a
Vlad Bokov <razum2um@mail.ru>
2018-11-05 09:04:16 +0700

    Proper bnf parser for rule incl. ip addresses & tests

--- src/iproute/route.clj
+++ src/iproute/route.clj
@@ -1,19 +1,28 @@
 (ns iproute.route
-  (:require [instaparse.core :as instaparse]))
+  (:require [instaparse.core :as instaparse]
+            [clojure.java.io :as io]))
 
-(def route-grammar
-  "route = <_>? (net | default) dev proto? scope? src?
-  net = ip mask?
-  default = <'default'> <_> <'via'> <_> ip <_>?
-  <mask> = <'/'> #'\\d{1,2}' <_>?
-  <ip> = #'\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}' <_>?
-  <n> = #'[0-9]+'
-  dev = <'dev'> <_> word <_>?
-  proto = <'proto'> <_> word <_>?
-  scope = <'scope'> <_> word <_>?
-  src = <'src'> <_> ip <_>?
-  <word> = #'[a-za-z0-9]+'
-  <_> = #'\\s'*")
+(defn- parse-int [s]
+  (BigInteger. s))
 
-(def route-parser (instaparse/parser route-grammar))
+(defn- with-parsed-int-key
+  "Makes transformation [:metric \"256\"] [:mtu \"1500\"] => [:metric 256] [:mtu 1500]"
+  [transform-map & keys]
+  (reduce #(assoc %1 %2 (juxt (constantly %2) parse-int)) transform-map keys))
 
+(defn- with-hashable-content
+  "Makes transformation [:src [:ip \"10.8.0.31\"]] => [:src {:ip \"10.8.0.31\"}]"
+  [transform-map & keys]
+  (reduce #(assoc %1 %2 (juxt (constantly %2) (fn [& args] (into {} args)))) transform-map keys))
+
+(def parser (instaparse/parser (io/resource "route.bnf")))
+
+(defn parsing [parse-fn x & opts]
+  (instaparse/transform (-> {:ip (fn [& args] [:ip (apply str args)])}
+                            (with-hashable-content :net :src :route)
+                            (with-parsed-int-key :metric :mtu :advmss :error :hoplimit)
+                            (assoc :default (fn [& args] [:default true])))
+                        (apply parse-fn parser x opts)))
+
+(def parses (partial parsing instaparse/parses))
+(def parse (partial parsing instaparse/parse))
