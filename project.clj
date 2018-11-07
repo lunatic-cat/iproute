e0011a9c893eca5defb0c7506fffa65f490c0d36
Vlad Bokov <razum2um@mail.ru>
2018-11-08 05:22:16 +0700

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iproute "0.1.4-SNAPSHOT"
+(defproject iproute "0.1.4"
   :description "iproute2 parser"
   :url "https://github.com/razum2um/iproute"
   :license {:name "Eclipse Public License"
